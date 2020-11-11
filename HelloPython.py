22593a23908dd6817432f843902cda15973e0211
Brian Good <brian.good@mycit.ie>
2020-11-11 22:59:05 +0000

    Test 4

--- HelloPython.py
+++ HelloPython.py
@@ -3,7 +3,4 @@ print("Hello world!")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))
-"Firt Jenkins commit"
-"Jenkins runs code"
-"Jenkins run code test 2"
-"Jenkins run code test 3"
+"Test 4"
